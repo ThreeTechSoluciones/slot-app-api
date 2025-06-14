0093652cdc87594ea5a18ef01403b918ccfd6335
Federico Sanchez <fede14.sanchez@gmail.com>
2025-06-13 23:54:13 -0300

    Create project

---
+++ src/test/java/com/three_tech_solutions/SlottApp/SlotAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.three_tech_solutions.SlottApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlotAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
