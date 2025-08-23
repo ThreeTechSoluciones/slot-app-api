56744c349a6219e4e9b0046178d525ab2409085b
Federico Sanchez <71854831+Fedesan14@users.noreply.github.com>
2025-08-23 14:02:20 -0300

    [FTR][SLOT-20]: add filters to get student (#23)

--- src/main/java/com/three_tech_solutions/slot_app/data/repositories/StudentRepository.java
+++ src/main/java/com/three_tech_solutions/slot_app/data/repositories/StudentRepository.java
@@ -1,9 +1,17 @@
 package com.three_tech_solutions.slot_app.data.repositories;
 
 import com.three_tech_solutions.slot_app.data.models.Student;
+import com.three_tech_solutions.slot_app.data.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface StudentRepository extends JpaRepository<Student, UUID> {
-    }
+    List<Student> findAllByUserAndNameContainingAndLastnameContainingAndDniContaining(
+            User user,
+            String studentName,
+            String studentLastname,
+            String studentDni
+    );
+}
