6cf0d1674a0fef5ae277fd77d408398cec3bcc75
LuciaRosales9 <luciarosalesb9@gmail.com>
2026-01-10 17:05:46 -0300

    [REF][SLOT-136]: Correcion

--- src/main/java/com/three_tech_solutions/slot_app/data/repositories/SlotRepository.java
+++ src/main/java/com/three_tech_solutions/slot_app/data/repositories/SlotRepository.java
@@ -13,13 +13,13 @@ import java.util.UUID;
 
 public interface SlotRepository extends JpaRepository<Slot, UUID> {
     @Query("""
-                SELECT COUNT(s) > 0
-                FROM Slot s
-                WHERE :dayOfWeek = s.dayOfWeek
-                  AND :time >= s.startTime
-                  AND :time <  s.endTime
-                  AND (:excludedSlotId IS NULL OR s.id <> :excludedSlotId)
-            """)
+        SELECT COUNT(s) > 0
+        FROM Slot s
+        WHERE :dayOfWeek = s.dayOfWeek
+            AND :time >= s.startTime
+            AND :time <  s.endTime
+            AND (:excludedSlotId IS NULL OR s.id <> :excludedSlotId)
+    """)
     boolean existsWithinRange(
             @Param("time") LocalTime time,
             @Param("dayOfWeek") DayOfWeek dayOfWeek,
@@ -27,13 +27,13 @@ public interface SlotRepository extends JpaRepository<Slot, UUID> {
     );
 
     @Query("""
-                SELECT s
-                FROM Slot s
-                WHERE s.user = :user
-                  AND s.active = true
-                  AND (:dayOfWeek IS NULL OR s.dayOfWeek = :dayOfWeek)
-                ORDER BY s.dayOfWeek ASC, s.startTime ASC
-            """)
+        SELECT s
+        FROM Slot s
+        WHERE s.user = :user
+            AND s.active = true
+            AND (:dayOfWeek IS NULL OR s.dayOfWeek = :dayOfWeek)
+            ORDER BY s.dayOfWeek ASC, s.startTime ASC
+    """)
     List<Slot> findAllByUserAndOptionalDayOfWeek(
             @Param("user") User user,
             @Param("dayOfWeek") DayOfWeek dayOfWeek
