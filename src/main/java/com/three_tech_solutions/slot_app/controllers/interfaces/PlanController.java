8f720aa548ebe2d3312756dce207d88b6fbc8162
Fedesan14 <fede14.sanchez@gmail.com>
2026-01-18 19:01:40 -0300

    [FTR][SLOT-145] add logic and add validation to create plan

--- src/main/java/com/three_tech_solutions/slot_app/controllers/interfaces/PlanController.java
+++ src/main/java/com/three_tech_solutions/slot_app/controllers/interfaces/PlanController.java
@@ -1,6 +1,7 @@
 package com.three_tech_solutions.slot_app.controllers.interfaces;
 
 import com.three_tech_solutions.slot_app.controllers.requests.CreatePlanRequest;
+import com.three_tech_solutions.slot_app.controllers.requests.UpdatePlanRequest;
 import com.three_tech_solutions.slot_app.controllers.requests.UpdatePriceRequest;
 import com.three_tech_solutions.slot_app.controllers.responses.PlanResponse;
 import jakarta.validation.Valid;
@@ -15,6 +16,9 @@ public interface PlanController {
 
     @PatchMapping("/{planId}/prices")
     PlanResponse updatePrice(@RequestBody @Valid UpdatePriceRequest updatePriceRequest, @PathVariable UUID planId);
+
+    @PatchMapping("/{planId}")
+    PlanResponse updatePlan(@RequestBody @Valid UpdatePlanRequest createPlanRequest, @PathVariable UUID planId);
 
     @DeleteMapping("/{planId}")
     void deletePlan(@PathVariable UUID planId);
