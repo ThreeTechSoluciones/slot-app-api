70cad205901f0ad2e4431a13bb199b6b4b5b70ac
Fedesan14 <fede14.sanchez@gmail.com>
2026-02-15 23:39:34 -0300

    [REF][SLOT-234] add refactor

--- src/main/java/com/three_tech_solutions/slot_app/services/implementations/StudentServiceImpl.java
+++ src/main/java/com/three_tech_solutions/slot_app/services/implementations/StudentServiceImpl.java
@@ -162,8 +162,17 @@ public class StudentServiceImpl implements StudentService {
         return new PageImpl<>(
                 filteredContent,
                 PageRequest.of(pageable.getPageNumber(), pageable.getPageSize()),
-                studentsPage.getTotalElements()
+                getContentSize(status, studentsPage, filteredContent)
         );
+    }
+
+    private static long getContentSize(StudentSituation status, Page<StudentResponse> studentsPage, List<StudentResponse> filteredContent) {
+        // This is because when we apply the student situation filter,
+        // we are filtering the content of the page, but the total elements of the page
+        // still corresponds to the total elements without applying the student situation filter.
+        // So we need to adjust the total elements of the page to correspond to the filtered content size.
+        long contentSize = status == null ? studentsPage.getTotalElements() : filteredContent.size();
+        return contentSize;
     }
 
     private static List<StudentResponse> getContentByStudentSituationFilter(StudentSituation status, Page<StudentResponse> studentsPage) {
