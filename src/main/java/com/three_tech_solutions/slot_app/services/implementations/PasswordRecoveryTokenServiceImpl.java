39522bd0b3559b1a8e6f2fb21ef165910b839b10
LuciaRosales9 <luciarosalesb9@gmail.com>
2026-03-30 13:12:45 -0300

    [FTR][SLOT-274]: update user password

--- src/main/java/com/three_tech_solutions/slot_app/services/implementations/PasswordRecoveryTokenServiceImpl.java
+++ src/main/java/com/three_tech_solutions/slot_app/services/implementations/PasswordRecoveryTokenServiceImpl.java
@@ -29,7 +29,7 @@ public class PasswordRecoveryTokenServiceImpl implements PasswordRecoveryTokenSe
                 .findByUsernameAndToken(username, token)
                 .orElseThrow(() -> new ResponseStatusException(BAD_REQUEST, "Token inválido"));
 
-        if (tokenEntity.getExpiresAt().isBefore(LocalDateTime.now())) {post
+        if (tokenEntity.getExpiresAt().isBefore(LocalDateTime.now())) {
             throw new ResponseStatusException(BAD_REQUEST, "Token expirado");
         }
 
