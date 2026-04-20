ca7fa0ddd1ee619ef3a62c7331b06fcd41ae0154
AbrilConrero <136394699+AbrilConrero@users.noreply.github.com>
2026-04-20 18:33:12 -0300

    [FTR][SLOT-266]: Implement code review feedback

--- src/main/java/com/three_tech_solutions/slot_app/data/models/Plan.java
+++ src/main/java/com/three_tech_solutions/slot_app/data/models/Plan.java
@@ -4,17 +4,13 @@ import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.web.server.ResponseStatusException;
-
 import java.time.LocalDate;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
-
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
-
-
 
 @Entity
 @Data
@@ -32,15 +28,12 @@ public class Plan {
     @Id
     UUID id = UUID.randomUUID();
 
-
-
     public Plan(String name, byte numberOfDays, List<Price> prices, User user) {
         this.name = name;
         this.numberOfDays = numberOfDays;
         this.prices = prices;
         this.user = user;
     }
-
 
     public Price getCurrentPrice() {
         LocalDate  today = LocalDate.now();
@@ -51,24 +44,23 @@ public class Plan {
                 .orElseThrow(() -> new ResponseStatusException(INTERNAL_SERVER_ERROR, "Hubo un error al obtener los precios de los planes"));
     }
 
-    public Optional <Price> getNextPrice (){
+    public Optional<Price> getNextPrice(){
         Price current = getCurrentPrice();
         return this.getPrices()
                 .stream()
-                .filter(price -> priceIsAfterCurrentPrice(current, price))
+                .filter(price -> startsAfter(current, price))
                 .min(Comparator.comparing(price -> price.startDate));
     }
 
-    public List <Price> getFuturePrices(){
+    public List<Price> getFuturePrices(){
         return getNextPrice()
                 .map(nextPrice -> this.getPrices()
                         .stream()
-                        .filter(price -> price.startDate.isAfter(nextPrice.startDate))
+                        .filter(price -> startsAfter(nextPrice, price))
                         .sorted(Comparator.comparing(price -> price.startDate))
                         .collect(Collectors.toList()))
                 .orElse(List.of());
     }
-
 
     private static boolean priceEndDateIsNullOrIsAfterToday(Price price, LocalDate today) {
         /*
@@ -78,10 +70,9 @@ public class Plan {
         return price.endDate == null || today.isBefore(price.endDate);
     }
 
-    private boolean priceIsAfterCurrentPrice (Price currentPrice, Price price){
-        return currentPrice.startDate.isBefore(price.startDate);
+    private boolean startsAfter(Price first, Price second){
+        return first.startDate.isBefore(second.startDate);
     }
-
 
     private static boolean priceStartDateIsBeforeOrEqualThanToday(Price price, LocalDate today) {
         return price.startDate.isBefore(today) || price.startDate.isEqual(today);
