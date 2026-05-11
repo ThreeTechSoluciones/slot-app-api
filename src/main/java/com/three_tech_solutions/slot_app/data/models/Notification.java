5efb3b7f50ceb42e306d7a0859515800fd4f9faf
LuciaRosales9 <luciarosalesb9@gmail.com>
2026-05-11 17:41:32 -0300

    [REF][SLOT-291]: Implementation of suggested changes

--- src/main/java/com/three_tech_solutions/slot_app/data/models/Notification.java
+++ src/main/java/com/three_tech_solutions/slot_app/data/models/Notification.java
@@ -1,9 +1,7 @@
 package com.three_tech_solutions.slot_app.data.models;
 
 import com.three_tech_solutions.slot_app.data.enums.NotificationType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -16,6 +14,7 @@ import java.util.UUID;
 public class Notification {
         LocalDate sendDate;
         String message;
+        @Enumerated(EnumType.STRING)
         NotificationType type;
         @ManyToOne
         User user;
