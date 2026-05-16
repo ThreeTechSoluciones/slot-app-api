97cd1861faca7f8d6703f6581ec5122282d78827
Lucía Rosales <135060623+LuciaRosales9@users.noreply.github.com>
2026-05-16 16:50:04 -0300

    [FTR][SLOT-291]: send notification of monthly fee expiration (#127)
    * [FTR][SLOT-291]: send notification of monthly fee expiration
    
    * [REF][SLOT]:Implementation of suggested changes
    
    * [REF][SLOT-291]: Implementation of suggested changes

--- src/main/java/com/three_tech_solutions/slot_app/cron_jobs/data/enums/CronJobType.java
+++ src/main/java/com/three_tech_solutions/slot_app/cron_jobs/data/enums/CronJobType.java
@@ -3,7 +3,8 @@ package com.three_tech_solutions.slot_app.cron_jobs.data.enums;
 public enum CronJobType {
     CREATE_UPCOMING_SPECIFIC_SLOTS("create_upcoming_specific_slots"),
     EXPIRE_PENDING_ABSENCES("expire_pending_absences"),
-    CREATE_STUDENTS_MONTHLY_FEES("create_students_monthly_fees");
+    CREATE_STUDENTS_MONTHLY_FEES("create_students_monthly_fees"),
+    EXPIRE_STUDENTS_MONTHLY_FEES("expire_students_monthly_fees");
 
     private final String value;
 
